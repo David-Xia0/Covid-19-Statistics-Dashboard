f308edab4f4f8ad3c08f20f9c664eba6d65446a3
David Xiao <davidjinxiao@hotmail.com>
2020-08-11 15:24:18 +1200

    now you can see graph as text, source matrix

--- Assignment2/dxia063.js
+++ Assignment2/dxia063.js
@@ -2,24 +2,48 @@ const getGraph = () => {
 
 
     const fetchPromise = fetch("http://redsox.uoa.auckland.ac.nz/ds/DairyService.svc/g",
-    
-    {
-        headers: {
-            "Accept": "application/json",
-        },
-    }
+
+        {
+            headers: {
+                "Accept": "application/json",
+            },
+        }
 
     );
 
-    const streamPromise = fetchPromise.then((response) => response.text());
+    const streamPromise = fetchPromise.then((response) => response.json());
     streamPromise.then((data) => {
 
-        
-        
 
-        let array = data.split("\n").join("<br>");
+        console.log(JSON.stringify(data));
 
-        document.getElementById("sourceMatrix").innerText = data;
+        let text = "<pre>[\n";
+
+        data.forEach( function(element1, index1, array1) {
+            text+="    [\n"
+
+            element1.forEach( function(element2, index2, array2) {
+
+                text+="        "+ element2;
+
+                if(index2 === array2.length - 1){
+                    text+="\n";
+                }else{
+                    text+=",\n";
+                }
+
+            })
+
+            if(index1 === array1.length -1){
+                text+="    ]\n";
+            }else{
+                text+="    ],\n";
+            }
+            
+              
+        })
+        text+="]</pre>";
+        document.getElementById("sourceMatrix").innerHTML = text;
     });
 
 }
